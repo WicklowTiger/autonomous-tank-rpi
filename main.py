5d847106075b07ac73c3dd6b7557ed8f3727ba38
WicklowTiger <razvan.miculit@gmail.com>
2022-01-09 19:48:21 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import logging
+from src.ActionManager import ActionManager
+
+
+if __name__ == "__main__":
+    logFormat = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=logFormat, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+    action_manager = ActionManager()
+    action_manager.run()
