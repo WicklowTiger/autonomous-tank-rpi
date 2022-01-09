628186000fefa8d93c550af2f37e15688378e2e7
WicklowTiger <razvan.miculit@gmail.com>
2022-01-09 19:50:24 +0200

    chore: created SocketReceiver module (binds to ethernet port and polls for messages)

---
+++ src/SocketReceiver.py
@@ -0,0 +1,24 @@
+import socket
+import time
+
+
+class SocketReceiver:
+    socket = None
+
+    def __init__(self, udp_ip: str, udp_port: int, message_queue):
+        self.udp_ip = udp_ip
+        self.udp_port = udp_port
+        self.message_queue = message_queue
+        # self.socket_setup()
+
+    def socket_setup(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind((self.udp_ip, self.udp_port))
+
+    def run(self):
+        print(self.__class__.__name__ + ": starting listener!")
+        while True:
+            # data, sender_address = self.socket.recvfrom(1024)
+            # print(f"Received message: {data} from {sender_address}")
+            time.sleep(2)
+            self.message_queue.update({'rotate': 90})
