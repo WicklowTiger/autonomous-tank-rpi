c8f948b079f6adc60c932d11da109fa77c3312ed
WicklowTiger <razvan.miculit@gmail.com>
2022-06-06 03:19:59 +0300

    refactor(conversion): message decoding now works differently

--- src/shared/conversion.py
+++ src/shared/conversion.py
@@ -1,8 +1,21 @@
+DETECTION = "DETECTION"
+ROTATE = "ROTATE"
+SHOOT = "SHOOT"
+MOVE_FORWARD = "MOVE_FORWARD"
+MOVE_BACKWARD = "MOVE_BACKWARD"
+MOVE_LEFT = "MOVE_LEFT"
+MOVE_RIGHT = "MOVE_RIGHT"
+
+possible_actions = [DETECTION, ROTATE, SHOOT, MOVE_FORWARD, MOVE_BACKWARD, MOVE_LEFT, MOVE_RIGHT]
 
 
 def decode_message(message: str) -> (str, int):
-    if message.__contains__("ROTATE"):
-        degree = message.upper().replace("ROTATE", "")
-        if degree.isnumeric():
-            return "rotate", int(degree)
-    return "not implemented", None
+    action_type = None
+    action_value = None
+    for action in possible_actions:
+        if message.startswith(action):
+            action_type = action
+            action_value = message[len(action)+1:]
+            break
+
+    return action_type, action_value
