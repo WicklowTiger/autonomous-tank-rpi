bc6ddf31bc76b888169930c9616b2e3060c45ad3
WicklowTiger <razvan.miculit@gmail.com>
2022-06-06 03:19:02 +0300

    refactoring(udp): moved udp config to it's own package

--- src/udp/SocketReceiver.py
+++ src/udp/SocketReceiver.py
@@ -1,6 +1,8 @@
 import socket
 
 from src.shared.conversion import decode_message
+from src.shared.indexing import get_action_index
+from src.decision.decision import add_detection
 
 
 class SocketReceiver:
@@ -27,4 +29,8 @@ class SocketReceiver:
             data, sender_address = self.socket.recvfrom(1024)
             print(f"Received message: {data} from {sender_address}")
             decoded = decode_message(data.decode("utf-8"))
-            self.message_queue.update({decoded[0]: decoded[1]})
+            if decoded[0] is not None:
+                if decoded[0] == "DETECTION":
+                    add_detection(decoded[1])
+                else:
+                    self.message_queue.update({get_action_index(): decoded})
