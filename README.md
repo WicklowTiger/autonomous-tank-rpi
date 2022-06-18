c6be3638219d826b06b88126f6fa5e97c73a34ed
WicklowTiger <razvan.miculit@gmail.com>
2022-06-18 21:38:26 +0300

    chore(proj): brought project up to date and beautified code

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # autonomous-tank-rpi
-Raspberry Pi part of Autonomous Military Vehicle project.
+Raspberry Pi part of Autonomous Targeting System project.
+
+Requirements: 
+- Raspberry Pi 4 B or earlier (> 3)
+- Python 3.7.3
--- src/udp/SocketReceiver.py
+++ src/udp/SocketReceiver.py
@@ -1,5 +1,8 @@
 import socket
+import threading
+import time
 
+from src.udp.config import CLI_ID, JETSON_ID, CONNECTION_TIMEOUT_LIMIT
 from src.shared.conversion import decode_message
 from src.shared.indexing import get_action_index
 from src.decision.decision import add_detection
@@ -12,21 +15,34 @@ class SocketReceiver:
     Received messages are stored in message_queue
     """
     socket = None
+    connections = {}
 
     def __init__(self, udp_ip: str, udp_port: int, message_queue):
         self.udp_ip = udp_ip
         self.udp_port = udp_port
         self.message_queue = message_queue
         self.socket_setup()
+        self.connections_setup()
+
+    def connections_setup(self):
+        self.connections = {"CLI": 0}
+        threading.Thread(target=self.run_timeout()).start()
 
     def socket_setup(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind((self.udp_ip, self.udp_port))
 
+    def cli_keep_alive(self):
+        self.connections.update({"CLI": CONNECTION_TIMEOUT_LIMIT})
+
     def run(self):
         print(self.__class__.__name__ + ": starting listener!")
         while True:
             data, sender_address = self.socket.recvfrom(1024)
+            if CLI_ID not in data and JETSON_ID not in data:
+                continue
+            elif CLI_ID in data:
+                self.cli_keep_alive()
             print(f"Received message: {data} from {sender_address}")
             decoded = decode_message(data.decode("utf-8"))
             if decoded[0] is not None:
@@ -34,3 +50,15 @@ class SocketReceiver:
                     add_detection(decoded[1])
                 else:
                     self.message_queue.update({get_action_index(): decoded})
+
+    def run_timeout(self):
+        while True:
+            for key, value in self.connections:
+                if value == 0:
+                    self.connections.pop(key)
+                    print(f"Timing out {key} connection")
+                    if key == "CLI":
+                        self.message_queue = {}
+                else:
+                    self.connections.update({key: value - 1})
+            time.sleep(0.5)
